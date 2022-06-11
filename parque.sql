b290d36ec9abebbaa4d4e8c4cf6b15b94231823d
Diego Zepeda <diego.zepeda.b87@gmail.com>
2022-06-11 13:36:34 +0000

    Segundo commit

--- parque.sql
+++ parque.sql
@@ -1,12 +1,12 @@
-CREATE TABLE cliente (
+CREATE TABLE clientes (
     rut_cliente NUMBER(11) NOT NULL,
     dv CHAR(1) NOT NULL,
-    primer
-
-
-
-
-
-
-
+    primer_nombre VARCHAR(250) NOT NULL,
+    segundo_nombre VARCHAR(250) ,
+    primer_paterno VARCHAR(250) NOT NULL,
+    segundo_materno VARCHAR(250) ,
+    direccion VARCHAR(250) NOT NULL,
+    fono NUMBER NOT NULL,
+    correo VARCHAR(250) NOT NULL,
+    comuna_id NUMBER NOT NULL,
 );
